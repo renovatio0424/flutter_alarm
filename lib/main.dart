6ec81883c76936665b4176ab735d54cb415d18a0
Reno <reno.kim@nexstreaming.com>
2019-07-19 18:12:41 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alarm/product_manager.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager('Food Tester'),
+      ),
+    );
+  }
+}
